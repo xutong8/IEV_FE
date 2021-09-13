80596ca17affd2a391551a9d96f97d601d23d3fd
xutong8 <2451836009@qq.com>
2021-09-13 15:31:45 +0800

    feat: stack添加legend

--- src/components/StackChart/index.tsx
+++ src/components/StackChart/index.tsx
@@ -16,6 +16,11 @@ import { useSVGSize } from "@/hooks/useSVGSize";
 import { processTicks } from "@/utils/processTicks";
 import { colorMap } from "@/utils/generateCountryColor";
 import dataSource from "@/data/nameToDigit2.json";
+import {
+  namesToColumns,
+  namesToNations,
+  nationsToNames,
+} from "@/utils/namesToColumns";
 
 export interface IStackChartProps {
   width: number | string;
@@ -29,14 +34,14 @@ const StackChart: React.FC<IStackChartProps> = (props) => {
 
   const [computedWidth, computedHeight] = useSVGSize(svgRef);
 
-  const legendHeight = 50;
+  const legendHeight = 125;
 
   // brush在Y轴上的偏移
-  const BrushYOffset = 20;
+  const BrushYOffset = 25;
 
   // (0, 0)点的位置
   const zeroPosition = useMemo(
-    () => [40, computedHeight - 60],
+    () => [40, computedHeight - 70],
     [computedHeight]
   );
 
@@ -105,8 +110,8 @@ const StackChart: React.FC<IStackChartProps> = (props) => {
     () =>
       brushX()
         .extent([
-          [zeroPosition[0], 20],
-          [computedWidth - 40, 40],
+          [zeroPosition[0], -20],
+          [computedWidth - 20, 0],
         ])
         .on("brush end", brushed),
     [computedWidth, zeroPosition, brushed]
@@ -148,54 +153,89 @@ const StackChart: React.FC<IStackChartProps> = (props) => {
     (digit2: string, state: boolean) => {
       // 更新过滤列表
       if (state) {
-        setAreaData(filterCountry([...filterList, digit2]));
-        setFilterList((filterList) => [...filterList, digit2]);
+        setAreaData(
+          filterCountry([
+            ...filterList,
+            namesToColumns.get(nationsToNames.get(digit2)),
+          ])
+        );
+        setFilterList((filterList) => [
+          ...filterList,
+          namesToColumns.get(nationsToNames.get(digit2)),
+        ]);
       } else {
-        filterList.splice(filterList.indexOf(digit2), 1);
+        filterList.splice(
+          filterList.indexOf(namesToColumns.get(nationsToNames.get(digit2))),
+          1
+        );
         setAreaData(filterCountry([...filterList]));
         setFilterList([...filterList]);
       }
     },
     [filterList]
   );
+
   return (
     <svg width={width} height={height} ref={svgRef}>
       <foreignObject width="100%" height={legendHeight}>
-        <Legend
-          orient="row"
-          data={dataSource.results.map((item) => item.name)}
-          color={(label: string) => {
-            const item = dataSource.results.find((item) => item.name === label);
-            return colorMap.get(item?.iso_2digit_alpha ?? "") ?? "";
-          }}
-          onClick={onClick}
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-        />
+        <div className={styles.legends}>
+          <Legend
+            orient="row"
+            data={dataSource.results.map((item) =>
+              namesToNations.get(item.name)
+            )}
+            color={(label: string) => {
+              const item = dataSource.results.find(
+                (item) => item.name === nationsToNames.get(label)
+              );
+              return colorMap.get(item?.iso_2digit_alpha ?? "") ?? "";
+            }}
+            onClick={onClick}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          />
+        </div>
       </foreignObject>
       <defs>
         <clipPath id="clip-path">
           <rect
             x={zeroPosition[0]}
-            y={36 + legendHeight}
+            y={legendHeight}
             width={
               computedWidth - 20 - zeroPosition[0] < 0
                 ? 0
                 : computedWidth - 20 - zeroPosition[0]
             }
-            // 56为Legend的高度
-            height={zeroPosition[1] - 36 < 0 ? 0 : zeroPosition[1] - 36}
+            height={
+              zeroPosition[1] - legendHeight < 0
+                ? 0
+                : zeroPosition[1] - legendHeight
+            }
+          />
+        </clipPath>
+        <clipPath id="clip-axis">
+          <rect
+            x={zeroPosition[0] - 10}
+            y={zeroPosition[1]}
+            width={
+              computedWidth - 20 - zeroPosition[0] + 25 < 0
+                ? 0
+                : computedWidth - 20 - zeroPosition[0] + 25
+            }
+            height={20}
           />
         </clipPath>
       </defs>
       <g>
-        <Axis
-          scale={xScale}
-          position={[0, zeroPosition[1]]}
-          direction={DirectionValue.BOTTOM}
-          tickValues={yearTicks}
-          ticks={X_TICKS}
-        />
+        <g clipPath="url(#clip-axis)">
+          <Axis
+            scale={xScale}
+            position={[0, zeroPosition[1]]}
+            direction={DirectionValue.BOTTOM}
+            tickValues={yearTicks}
+            ticks={X_TICKS}
+          />
+        </g>
         <Axis
           scale={yScale}
           position={[zeroPosition[0], 0]}
@@ -221,10 +261,10 @@ const StackChart: React.FC<IStackChartProps> = (props) => {
           })}
         </g>
       </g>
-      <g transform={`translate(0, ${zeroPosition[1] + BrushYOffset})`}>
+      <g transform={`translate(0, ${zeroPosition[1] + BrushYOffset + 20})`}>
         <Axis
           scale={brushScale}
-          position={[0, 41]}
+          position={[0, 0]}
           direction={DirectionValue.BOTTOM}
           tickValues={yearTicks}
           ticks={X_TICKS}
