9bc7c1418118c89a391bca6fb18f513b4885fe6c
xutong8 <2451836009@qq.com>
2021-09-26 15:16:32 +0800

    feat: finish

--- src/components/HeatMap/index.tsx
+++ src/components/HeatMap/index.tsx
@@ -1,6 +1,9 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { scaleBand, scaleLinear } from "d3-scale";
 import { IRow } from "@/types/heatmap";
+import { useSelector } from "react-redux";
+import { IStore } from "@/reducers";
+import { isEqual } from "lodash";
 export interface IHeatMapProps {
   width: number;
   height: number;
@@ -37,6 +40,17 @@ const HeatMap: React.FC<IHeatMapProps> = (props) => {
   // y scale
   const yScale = scaleBand().domain(countryNames).range([0, height]);
 
+  // countryList selector
+  const countryList = useSelector(
+    (state: IStore) => state.countryList,
+    (prev, next) => isEqual(prev, next)
+  );
+
+  // 当前hover的countryName
+  // const [countryName, setCountryName] = useState<string>("");
+  const [currentRowIdx, setCurrentRowIdx] = useState<number>(-1);
+  const [currentColumnIdx, setCurrentColumnIdx] = useState<number>(-1);
+
   return (
     <svg
       width={width}
@@ -44,10 +58,15 @@ const HeatMap: React.FC<IHeatMapProps> = (props) => {
       transform="rotate(45)"
       className={className}
     >
-      {dataSource.map((row) => {
+      <defs>
+        <filter height="200%" width="200%" x="-50%" y="-50%" id="f1">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="10" />
+        </filter>
+      </defs>
+      {dataSource.map((row, rowIdx) => {
         return (
           <g key={row.countryName}>
-            {row.explist.map((item) => {
+            {row.explist.map((item, columnIdx) => {
               return (
                 <rect
                   id={item.countryName}
@@ -59,10 +78,35 @@ const HeatMap: React.FC<IHeatMapProps> = (props) => {
                       ? "#ffffff"
                       : colorScale(item.expvalue)
                   }
+                  fillOpacity={
+                    countryList.length === 0 ||
+                    countryList.find(
+                      (country) =>
+                        country.name === item.countryName ||
+                        country.name === row.countryName
+                    )
+                      ? 1
+                      : 0.3
+                  }
                   strokeWidth={1}
                   stroke="#d3d3d2"
                   width={width / countryNames.length}
                   height={height / countryNames.length}
+                  onMouseEnter={() => {
+                    setCurrentRowIdx(rowIdx);
+                    setCurrentColumnIdx(columnIdx);
+                  }}
+                  onMouseLeave={() => {
+                    setCurrentRowIdx(-1);
+                    setCurrentColumnIdx(-1);
+                  }}
+                  filter={
+                    (currentRowIdx === -1 && currentColumnIdx === -1) ||
+                    rowIdx === currentRowIdx ||
+                    columnIdx === currentColumnIdx
+                      ? undefined
+                      : "url(#f1)"
+                  }
                 />
               );
             })}
